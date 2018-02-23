9bb823f000c60f02efe9a8f56b0e3e4a3f7fbb49
Matt Kimber <matt.kimber@collinsongroup.com>
2018-02-23 10:59:24 +0000

    Roll our own Kibana to match OSS version of Elastic Search

--- uptime_prospector/src/index.js
+++ uptime_prospector/src/index.js
@@ -1,4 +1,5 @@
-
+const elasticsearch = require("elasticsearch");
+const request = require("request-promise-native");
 
 var getEvent = (time, err, route) => {
 	return {
@@ -6,9 +7,45 @@ var getEvent = (time, err, route) => {
 		type: "ping",
 		error: err,
 		time: time,
+		datestamp: new Date(),
 		ucr: null,
 		route: route
 	};
 }
 
-console.log(JSON.stringify(getEvent(100, null, "/test")));
+var client = new elasticsearch.Client({
+	host: process.env.ES_HOST || "localhost:9200"
+})
+
+var checkEndpoint = function(endpoint) {
+	var start_time = new Date().getTime();
+
+	return request({
+		uri: endpoint.request.uri,
+		method: endpoint.request.verb,
+		headers: endpoint.request.headers || {},
+		body: endpoint.request.body,
+		timeout: 8000
+	}).then(data => {
+		var duration = new Date().getTime() - start_time;
+		return Promise.resolve(getEvent(duration, null, endpoint.request.uri));
+	}).catch(err => {
+		return Promise.resolve(getEvent(duration, err, endpoint.request.uri));
+	})
+}
+
+var indexEvents = function(event) {
+	return client.index({
+		index: "pings",
+		type: "event",
+		body: event
+	});
+}
+
+checkEndpoint({
+	request: {
+		uri: "http://www.google.com/",
+		method: "GET"
+	}
+}).then(indexEvents)
+.then(() => console.log("Done"))
